1d7582238300aadc266d099f7cce7a658a4d61e8
João Pedro Macedo Hong <107969352+JoaoHong@users.noreply.github.com>
2024-08-15 18:29:13 -0300

    Correcao do program

--- JoaoHong.APICollection.API/Program.cs
+++ JoaoHong.APICollection.API/Program.cs
@@ -4,45 +4,62 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 
-var builder = WebApplication.CreateBuilder(args);
-
-var configuration = builder.Configuration;
-
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(x =>
-    {
-        x.RequireHttpsMetadata = false;
-        x.SaveToken = true;
-        x.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JwtSettings:Key"])),
-            ValidateIssuer = false,
-            ValidateAudience = false,
-            ClockSkew = TimeSpan.Zero
-        };
-    });
-
-builder.Services.AddControllers();
-builder.Services.AddHttpClient();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-InfrastructureServices.AddInfrastructureServices(builder.Services);
-ApplicationServices.AddApplicationServices(builder.Services);
-
-var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
+namespace JoaoHong.APICollection.API
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
+
+			var configuration = builder.Configuration;
+
+			builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+				.AddJwtBearer(x =>
+				{
+					x.RequireHttpsMetadata = false;
+					x.SaveToken = true;
+					x.TokenValidationParameters = new TokenValidationParameters
+					{
+						ValidateIssuerSigningKey = true,
+						IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JwtSettings:Key"])),
+						ValidateIssuer = false,
+						ValidateAudience = false,
+						ClockSkew = TimeSpan.Zero
+					};
+				});
+
+			builder.Services.AddControllers();
+			builder.Services.AddHttpClient();
+			builder.Services.AddEndpointsApiExplorer();
+			builder.Services.AddSwaggerGen();
+			builder.Logging.ClearProviders();
+			builder.Logging.AddConsole();
+			builder.Logging.AddDebug();
+
+			InfrastructureServices.AddInfrastructureServices(builder.Services);
+			ApplicationServices.AddApplicationServices(builder.Services);
+
+			var app = builder.Build();
+
+			if (app.Environment.IsDevelopment())
+			{
+				
+			}
+
+			app.UseSwagger();
+			app.UseSwaggerUI(c =>
+			{
+				c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+				c.RoutePrefix = string.Empty;
+			});
+
+			app.UseHttpsRedirection();
+			app.UseAuthentication();
+			app.UseAuthorization();
+			app.MapControllers();
+
+			app.Run();
+		}
+	}
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
